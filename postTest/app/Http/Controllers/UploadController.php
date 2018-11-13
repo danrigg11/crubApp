b019ec5ddbc9610163b565a757aef5bc7a76f1c6
Daniel <dan_rigg11@hotmail.com>
2018-11-13 16:19:11 +0000

    project

---
+++ postTest/app/Http/Controllers/UploadController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+
+class UploadController extends Controller {
+
+	public function upload(){
+
+		if(Input::hasFile('file')){
+
+			echo 'Uploaded';
+			$file = Input::file('file');
+			$file->move('uploads', $file->getClientOriginalName());
+			echo '';
+		}
+
+	}
+}
---
+++ postTest/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PostsController@show');
+
+Route::get('/posts', 'PostsController@index')->name('posts.index');
+Route::get('/posts/details/{id}', 'PostsController@details')->name('posts.details');
+Route::get('/posts/add', 'PostsController@add')->name('posts.add');
+Route::post('/posts/insert', 'PostsController@insert')->name('posts.insert');
+Route::get('/posts/edit/{id}', 'PostsController@edit')->name('posts.edit');
+Route::post('/posts/update/{id}', 'PostsController@update')->name('posts.update');
+Route::get('/posts/delete/{id}', 'PostsController@delete')->name('posts.delete');
+
+Route::post('upload', 'UploadController@upload');
+
+Route::any('images', function() {
+
+    $images = '';
+
+    foreach (File::allFiles(public_path() . 'uploads') as $file)
+    {
+        $filename = $file->getRelativePathName();
+
+        $images .= HTML::image('public/uploads/'.$filename, $filename);
+    }
+
+    return "<htm><body>$images</body></htm>";
+
+});
